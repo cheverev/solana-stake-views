0f65a0a2b0e0458cbf8bfc531389771b1ee9b681
Voland <54623455+cheverev@users.noreply.github.com>
2022-09-27 09:53:21 +0300

    Update solana.py
    add deactivating epoch/stake

--- solana.py
+++ solana.py
@@ -20,25 +20,31 @@ def solana():
         stake_sum = 0
         activating = 0
         activationEpoch = 0
+        deactivationEpoch = 0
+        deactivatingStake = 0
         for elem in range(len(sol_list)):
             if withdrawer == sol_list[elem].get('withdrawer'):
                 quantity += 1
                 stake_sum += sol_list[elem].get('accountBalance')
-                if sol_list[elem].get('activatingStake'):
+                if sol_list[elem].get('activating'):
                     activating += sol_list[elem].get('activatingStake')
                     activationEpoch = sol_list[elem].get("activationEpoch")
+                if sol_list[elem].get('deactivatingStake'):
+                    deactivationEpoch = sol_list[elem].get("deactivationEpoch")
+                    deactivatingStake += sol_list[elem].get('deactivatingStake')
         activating /= 1000000000
         stake_sum /= 1000000000
-        stake_res.append({'withdrawer': withdrawer, 'quantity': quantity, 'stake_sum': stake_sum, 'activating': activating, 'activationepoch': activationEpoch})
+        deactivatingStake /= 1000000000
+        stake_res.append({'withdrawer': withdrawer, 'quantity': quantity, 'stake_sum': stake_sum, 'activating': activating, 'activationepoch': activationEpoch, 'deactivationEpoch': deactivationEpoch, 'deactivatingStake': deactivatingStake})
         stake_res.sort(key=lambda d: d['stake_sum'], reverse=True)
-        print("\n")
+    print("\n")
     for stake in stake_res:
         print(f"withdrawer key {stake.get('withdrawer')}")
         print(f"Number of steaks {stake.get('quantity')} for the amount {stake.get('stake_sum')}")
-        if stake.get('activating') == 0.0:
-            pass
-        else:
+        if stake.get('activating') != 0.0:
             print(f"Activating: {stake.get('activating')} on {stake.get('activationepoch')}")
+        if stake.get('deactivatingStake') != 0.0:
+            print(f"Deactivating: {stake.get('deactivatingStake')} on {stake.get('deactivationEpoch')}")
         print("-"*60)
 
 if __name__ == '__main__':
